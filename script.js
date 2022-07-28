e36f684959e49291b04ebf5a74a1df107245fe88
Suraj <mouryasuraj626444@gmail.com>
2022-07-28 17:28:59 +0530

    Updated the MyWork Section(Portfolio)

--- script.js
+++ script.js
@@ -3,13 +3,30 @@
 var tabTitles = document.getElementsByClassName("tab-title");
 var tabContents = document.getElementsByClassName("tab-contents");
 
-function opentab(tabname){
-    for(let tabTitle of tabTitles){
+function opentab(tabname) {
+    for (let tabTitle of tabTitles) {
         tabTitle.classList.remove("active-title");
     }
-    for(let tabContent of tabContents){
+    for (let tabContent of tabContents) {
         tabContent.classList.remove("active-tab");
     }
     event.currentTarget.classList.add("active-title")
     document.getElementById(tabname).classList.add("active-tab")
 }
+
+var hiddenwork1 = document.getElementById("hidden-work1");
+var hiddenwork2 = document.getElementById("hidden-work2");
+var button = document.getElementById("btn");
+function showmore() {
+    if(hiddenwork1.style.display === "block" && hiddenwork2.style.display === "block"){
+        hiddenwork1.style.display = "none";
+        hiddenwork2.style.display = "none";
+        button.innerHTML = "Show more";
+    }
+    else{
+        hiddenwork1.style.display="block";
+        hiddenwork2.style.display="block";
+        button.innerHTML = "Show less";
+    }
+}
+
